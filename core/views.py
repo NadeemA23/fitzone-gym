63931bbe08481d83f2247e1a78c357d2815d47c5
NadeemA23 <105326841+NadeemA23@users.noreply.github.com>
2026-09-15 05:20:09 +0000

    Add user registration

--- core/views.py
+++ core/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     """Display the FitZone Gym homepage."""
     return render(request, 'core/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        'core/register.html',
+        {'form': form}
+    )
+
+
+   
