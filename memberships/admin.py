047225fc605c97de18980ab6cae079cf17364fa0
NadeemA23 <105326841+NadeemA23@users.noreply.github.com>
2026-09-25 08:42:26 +0000

    Add user membership model

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MembershipPlan
-
+from .models import MembershipPlan, UserMembership
 
 admin.site.register(MembershipPlan)
+admin.site.register(UserMembership)
--- memberships/models.py
+++ memberships/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class MembershipPlan(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +9,13 @@ class MembershipPlan(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserMembership(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plan = models.ForeignKey(MembershipPlan, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.plan.name}"
