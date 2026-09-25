8f5b7835ee2806456f26f25c2be528bfecc40868
NadeemA23 <105326841+NadeemA23@users.noreply.github.com>
2026-09-25 09:06:43 +0000

    Add memberships plan selection

--- memberships/urls.py
+++ memberships/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.membership_plans, name='membership_plans'),
+     path('select/<int:plan_id>/', views.select_plan, name='select_plan'),
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import MembershipPlan
 
 
@@ -9,4 +10,13 @@ def membership_plans(request):
         request,
         'memberships/membership_plans.html',
         {'plans': plans}
+    )
+@login_required
+def select_plan(request, plan_id):
+    plan = get_object_or_404(MembershipPlan, id=plan_id)
+
+    return render(
+        request,
+        'memberships/select_plan.html',
+        {'plan': plan}
     )
